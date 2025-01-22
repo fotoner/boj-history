88d249a9aa2acec60eb37fbc5373d2fae8d2383f
Hyeonjae Nam <jaemuon5582@gmail.com>
2025-01-23 00:19:43 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();        
+        int answer = 1;
+        
+        for(String[] clothe: clothes) {
+            map.put(clothe[1], map.getOrDefault(clothe[1], 1) + 1);
+        }
+        
+        
+        for(String key: map.keySet()){
+            answer *= map.get(key);
+        }
+        
+        return answer - 1;
+    }
+}

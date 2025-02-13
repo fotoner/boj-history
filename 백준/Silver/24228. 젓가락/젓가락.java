9eb0817cf2e03ee6c8f15d3f460902df8beae24d
Hyeonjae Nam <jaemuon5582@gmail.com>
2025-02-14 00:57:59 +0900

    [Silver V] Title: 젓가락, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Silver/24228. 젓가락/젓가락.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long n = Long.parseLong(st.nextToken());
+        long r = Long.parseLong(st.nextToken());
+        System.out.println(n + r * 2 - 1);
+    }
+}

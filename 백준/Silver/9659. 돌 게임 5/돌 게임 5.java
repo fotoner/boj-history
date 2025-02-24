4f41cf1e412bdcfe1e7678545b864ce56945c9de
Hyeonjae Nam <jaemuon5582@gmail.com>
2025-02-24 22:21:48 +0900

    [Silver III] Title: 돌 게임 5, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/9659. 돌 게임 5/돌 게임 5.java
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.java
@@ -1,11 +1,10 @@
 import java.io.*;
-import java.math.BigInteger;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BigInteger n = new  BigInteger(br.readLine());
+        String n = br.readLine();
 
-        System.out.println(n.mod(BigInteger.valueOf(2)).intValue() == 1 ? "SK" : "CY");
+        System.out.println((n.charAt(n.length() - 1) - '0') % 2 == 1 ? "SK" : "CY");
     }
 }

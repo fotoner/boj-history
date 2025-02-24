3bd69dba952559d2163e613bed0033a57d4a39d5
Hyeonjae Nam <jaemuon5582@gmail.com>
2025-02-24 23:25:11 +0900

    [Silver II] Title: 유턴 싫어, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/2823. 유턴 싫어/유턴 싫어.java
+++ 백준/Silver/2823. 유턴 싫어/유턴 싫어.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 class Main {
     static int[][] map;
-    static boolean[][] visited;
     static int height;
     static int width;
 
@@ -14,7 +13,6 @@ class Main {
         width = Integer.parseInt(st.nextToken());
 
         map = new int[height + 2][width + 2];
-        visited = new boolean[height + 2][width + 2];
 
         Arrays.fill(map[0], -1);
         Arrays.fill(map[height + 1], -1);
@@ -29,45 +27,31 @@ class Main {
                 map[i][j] = arr[j - 1] == 'X' ? 1 : 0;
             }
         }
-        boolean flag = false;
-        Queue<int[]> q = new LinkedList<>();
 
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, -1, 0, 1};
 
         for (int i = 1; i <= height ; i++) {
             for (int j = 1; j <= width ; j++) {
-                if(visited[i][j] || map[i][j] != 0)
+                if(map[i][j] != 0)
                     continue;
 
-                q.add(new int[]{i, j});
+                int count = 0;
+                for (int k = 0; k < 4; k++) {
+                    int nextX = j + dx[k];
+                    int nextY = i + dy[k];
 
-                while (!q.isEmpty()) {
-                    int[] curPos = q.poll();
-                    int x = curPos[1];
-                    int y = curPos[0];
-
-                    visited[y][x] = true;
-
-                    int count = 0;
-                    for (int k = 0; k < 4; k++) {
-                        int nextX = x + dx[k];
-                        int nextY = y + dy[k];
-
-                        if(map[nextY][nextX] == 0){
-                            count++;
-                            if(!visited[nextY][nextX]) {
-                                q.add(new int[]{nextY, nextX, k});
-                            }
-                        }
+                    if(map[nextY][nextX] == 0){
+                        count++;
                     }
-                    if(count < 2) {
-                        flag = true;
-                    }
+                }
+                if(count < 2) {
+                    System.out.println(1);
+                    return;
                 }
             }
         }
 
-        System.out.println(flag? 1 : 0);
+        System.out.println(0);
     }
 }

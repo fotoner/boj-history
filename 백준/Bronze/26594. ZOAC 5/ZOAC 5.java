c60ec1a326b3f071aa858e1e95d35d0a1d88e01a
Hyeonjae Nam <jaemuon5582@gmail.com>
2025-03-12 23:49:12 +0900

    [Bronze III] Title: ZOAC 5, Time: 212 ms, Memory: 20156 KB -BaekjoonHub

---
+++ 백준/Bronze/26594. ZOAC 5/ZOAC 5.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        char c = str.charAt(0);
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == c) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        System.out.println(count);
+    }
+}
